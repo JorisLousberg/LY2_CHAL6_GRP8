60899d7b5c94d021f41bca4d1774940b7b589e5e
Even70000 <75841107+Even70000@users.noreply.github.com>
2022-02-02 12:31:03 +0100

    about us

--- trashweb/about_us.php
+++ trashweb/about_us.php
@@ -13,6 +13,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="./css/mainstyle.css">
      <link rel="stylesheet" href="css/navbarstyle.css?v=6">
+     <link rel="stylesheet" href="css/aboutStyle.css">
      <title>Document</title>
  </head>
  <body>
@@ -21,9 +22,31 @@
             <?php require_once("includes/menu.inc.php") ?>
         </nav>
         <div class="middiv">
-        MAIN LAYOUT-1
-layout (navbar, title, infoblock(text + goal), donationblock(donation info + donation goals(amount + short desc),
- fake counter, donate button(no function) ))
+          <div id='titelBlock'>
+              <div id='titelContent'>
+                 <h1 id='titelInfo'>Over ons</h1>
+              </div>
+          </div>
+           
+          <div id='infoBlock'>
+             <div id='infoContent'>
+                 <p id='infoPara'>
+                 Leerlingen laten te veel afval liggen, dit gebeurd zowel binnen in het gebouw als buiten op het schoolterrein. Een reden hiervan is dat de afvalbakken die nu in gebruik zijn eigenlijk niet goed zijn, vanwege het feit dat ze zo open zijn proberen veel leerlingen het gewoon erin te ‘’gooien’’.                                   Als dat ze dan niet lukt dan laten ze het gewoon liggen. 
+                 Verder is er ook in probleem met het ontwerp van de afvalbakken in verband met dat als het hard waait al het afval uit de afvalbakken waait en over de grond verspreid.                                                                 Dat is minder gunstig voor zowel het personeel als voor fatsoenlijke afvalscheiding. 
+                 Daarover gesproken: de afvalscheiding moet verbeterd worden vergeleken met wat het nu is, want nu doen zowel leerlingen als personeel alles in dezelfde prullenbakken proppen.
+
+                 </p>
+             </div>
+          </div>
+
+          <div id='donatieBlock'>
+              <div id='donatieContent'>
+                  <div id='donatieInfo'>jklklkl</div>
+                  <div id='donantieBtn'>llhlj</div>
+                  <div id='donatieGoals'>hljl</div>
+              </div>
+          </div>
+
         </div>
         </div>
         <div class="lowerdiv"></div>
