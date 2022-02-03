4a6c51e60cb14e0712e77f0796206f11d8d8936a
galactorsus( <504972@vistacollege.nl>
2022-02-03 13:19:17 +0100

    game page lay out fix

--- trashweb/trash_masterz.php
+++ trashweb/trash_masterz.php
@@ -8,22 +8,26 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="./css/mainstyle.css">
+
      <link rel="stylesheet" href="css/navbarstyle.css?v=6">
      <link rel="stylesheet" href="css/mainstyle2.css">
      
      <title>Document</title>
  </head>
  <body>
-    <nav>
-        <?php require_once("includes/menu.inc.php") ?>
-    </nav>
-   
-    <canvas id="canvas">
-  
-    <!-- <img id="image" src="https://m1.behance.net/rendition/modules/74723571/disp/0beedf48c9a000c812866297384c0cf4.png" style="display: none">!-->
- 
-    </canvas>
+    <div class="mainbody">
+        <nav>
+            <?php require_once("includes/menu.inc.php") ?>
+        </nav>
+        <div class="middiv">
+                <canvas id="canvas">
+                </canvas>
+        </div>
+        <div class="lowerdiv"></div>
+        </div>
+    </div>
+    
+    
     <script src='./js/game.js'></script>
  </body>
  </html
